0f96a01200fc4420f880a9fa5964825bfc06ac8c
Carlo Sganzerla <carlosganzrl@hotmail.com>
2023-09-05 19:07:40 -0300

    levei porrada agora

--- codechef/fgfs.py
+++ codechef/fgfs.py
@@ -1,37 +1,22 @@
-class Tree:
-    def __init__(self, s, f) -> None:
-        self.left = None
-        self.right = None
-        self.f = f
-        self.s = s
-        self.available = 1
-
-    def _add(self, s, f, acc):
-        if s < self.f and self.s < self.f:
-            if self.left:
-                return self.left._add(s, f, acc)
-            else:
-                self.left = Tree(s, f)
-                return acc
-        else:
-            if self.right:
-                return self.right._add(s, f, acc + 1)
-            else:
-                self.right = Tree(s, f)
-                return acc + 1
-
-    def add(self, s, f):
-        self.available = max(self.available, self._add(s, f, 1))
-
 def ans(N, K, C):
-    trees = {}
+    compartments = {}
+    ans = 0
     for s, f, k in C:
-        if k not in trees:
-            trees[k] = Tree(s, f)
+        if k not in compartments:
+            compartments[k] = [(f, s)]
         else:
-            trees[k].add(s, f)
+            compartments[k].append((f, s))
 
-    return sum([t.available for t in trees.values()])
+    for c in compartments.values():
+        c.sort()
+        max_val = 0
+        for f, s in c:
+            if s >= max_val:
+                ans += 1
+                max_val = f
+
+
+    return ans
 
 
 if __name__ == "__main__":
